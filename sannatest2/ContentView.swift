f6bd6914d13cbb0c5124bcf6ec5efbe83d643e97
Sanna Tupala <sanna.tupalagmail.com>
2023-10-11 10:56:05 +0200

    Initial Commit

---
+++ sannatest2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sannatest2
+//
+//  Created by Sanna Tupala on 2023-10-11.
+//  Copyright © 2023 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
